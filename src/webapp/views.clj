9a74c9cc4c009a68f76cce316a098297d4608de0
Max Kolle <mkolle@elcom.com>
2021-08-05 12:50:14 +0100

    Fix webapp db, create db

--- src/webapp/views.clj
+++ src/webapp/views.clj
@@ -6,7 +6,6 @@
 
 (defn gen-page-head
   [title]
-
   [:head
    [:title (str "Locations: " title)]
    (page/include-css "/css/styles.css")])
@@ -16,7 +15,7 @@
    "[ "
    [:a {:href "/"} "Home"]
    " | "
-   [:a {:href "/add-locations"} "Add a Location"]
+   [:a {:href "/add-location"} "Add a Location"]
    " | "
    [:a {:href "/all-locations"} "View all Locations"]
    " ]"])
@@ -34,24 +33,24 @@
   (page/html5
     (gen-page-head "Add a Location")
     header-links
-    [:h1 "Add a location"]
+    [:h1 "Add a Location"]
     [:form {:action "/add-location" :method "POST"}
-     (util/anti-forgery-field)
-     [:p "x: " [:input {:type "text" :name "x"}]]
-     [:p "y: " [:input {:type "text" :name "y"}]]
+     (util/anti-forgery-field) ; prevents cross-site scripting attacks
+     [:p "x value: " [:input {:type "text" :name "x"}]]
+     [:p "y value: " [:input {:type "text" :name "y"}]]
      [:p [:input {:type "submit" :value "submit location"}]]]))
-
 
 (defn add-location-results-page
   [{:keys [x y]}]
   (let [id (db/add-location-to-db x y)]
     (page/html5
-      (gen-page-head "Added a location")
+      (gen-page-head "Added a Location")
       header-links
-      [:h1 "Added a location"]
-      [:p "Added [" x ", " y "] (id: " id ") to the db."
-       [:a {:href (str "/location/" id)} "Check"]
+      [:h1 "Added a Location"]
+      [:p "Added [" x ", " y "] (id: " id ") to the db. "
+       [:a {:href (str "/location/" id)} "See for yourself"]
        "."])))
+
 
 
 (defn location-page
